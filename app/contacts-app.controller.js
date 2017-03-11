e1f01553eed686ccef7c44bd98d98318c9510549
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2017-03-11 09:21:57 +0100

    list

--- app/contacts-app.controller.js
+++ app/contacts-app.controller.js
@@ -1,4 +1,27 @@
 angular.module('contactsApp')
     .controller('homeCtrl', function ($scope) {
-        console.log('hello');
+
+        $scope.contacts = [
+            {
+                "name": "Adkins Henson",
+                "company": "COMBOGEN",
+                "email": "adkinshenson@combogen.com"
+            },
+            {
+                "name": "Jan Salinas",
+                "company": "EQUICOM",
+                "email": "jansalinas@equicom.com"
+            },
+            {
+                "name": "Kay Montoya",
+                "company": "XINWARE",
+                "email": "kaymontoya@xinware.com"
+            },
+            {
+                "name": "Lola Franks",
+                "company": "ANOCHA",
+                "email": "lolafranks@anocha.com"
+            }
+        ];
+
     });
