1b421fde5d770d236f8d7118566c54963fe0dd9b
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2017-03-11 11:37:03 +0100

    routing

--- app/contacts-app.module.js
+++ app/contacts-app.module.js
@@ -1,1 +1,16 @@
-angular.module('contactsApp', []);
+angular.module('contactsApp', [
+    'ui.router'
+])
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'homeCtrl',
+                templateUrl: 'app/templates/home.html'
+            });
+
+    });
